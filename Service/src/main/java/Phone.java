cc3bbbbf6b56ce7514e1c6d6fb003e5e00c26cd0
Nikita <nikiteveselovu@gmail.com>
2022-10-12 23:42:17 +0300

    initial

--- Service/src/main/java/Phone.java
+++ Service/src/main/java/Phone.java
@@ -3,7 +3,7 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.Value;
 
-@Value
+@AllArgsConstructor
 public class Phone {
     @NonNull public String prefix;
     @NonNull public String number;
